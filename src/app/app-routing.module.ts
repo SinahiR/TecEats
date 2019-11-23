d70c99de322142aacb95636c15af9a6fe5e67c1c
sinahiruiz <siruizro@ittepic.edu.mx>
2019-11-22 23:47:28 -0700

    Creacion del nav, carrousel, cards
    Se agrego un nav con el logo de TecEats, un carrousel NO FUNCIONAL por el momento y cards con informacion de la comida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//IMPORTAR
+import {HomeComponent } from './componentes/home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+  path:'',
+  redirectTo:'home',
+  pathMatch:'full'
+},
+{
+  path:'home',
+  component: HomeComponent
+}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
