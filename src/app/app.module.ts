2610942dfdff5ff3d322750461d55082084aa7c8
Alejandra Grande <gaalgrandede@ittepic.edu.mx>
2019-11-25 17:11:34 -0700

    ParteProductos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './componentes/home/home.component';
+import { AgregarproductosComponent } from './componentes/agregarproductos/agregarproductos.component';
+import { CrearproductosComponent } from './componentes/crearproductos/crearproductos.component';
+import { EditarproductoComponent } from './componentes/editarproducto/editarproducto.component';
 
 //IMPORTAR
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AgregarproductosComponent,
+    CrearproductosComponent,
+    EditarproductoComponent
   ],
   imports: [
     BrowserModule,
