476ab3082f630c883971fb93ce6583522891737d
Alejandra Grande <gaalgrandede@ittepic.edu.mx>
2019-11-27 12:22:59 -0700

    diseño

--- src/app/componentes/home/home.component.spec.ts
+++ src/app/componentes/home/home.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core';
 
 import { HomeComponent } from './home.component';
 
